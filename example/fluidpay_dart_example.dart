2bc5806f368674dcd5a560ef9e43c414a1dea0a6
jkirtyan <jkirtyan@gmail.com>
2023-01-03 15:34:50 +0100

    Extended readme with requests and responses; Added a few lines for example class;

--- example/fluidpay_dart_example.dart
+++ example/fluidpay_dart_example.dart
@@ -1,1 +1,22 @@
-void main() {}
+import 'package:fluidpay/com/fluidpay/authentication/request.dart';
+import 'package:fluidpay/com/fluidpay/gateway.dart';
+import 'package:fluidpay/com/fluidpay/transaction/request.dart';
+
+void main() async {
+  Gateway.instance.init('https://example.com/api/v2', apiKey: 'YourApiKey');
+
+  final response = await Gateway.instance.login(AuthLoginRequest('testUser', 'testPassword'));
+
+  if ((response.statusCode ?? 0) / 100 != 2) {
+    print('Login was not successful');
+    return;
+  }
+
+  final transactions = await Gateway.instance.execute(TransactionSearchRequest());
+
+  if (transactions.data != null) {
+    for (final transaction in transactions.data!) {
+      print(transaction.orderId);
+    }
+  }
+}
